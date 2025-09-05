25e6a2acb1aff87be1011c7f097781a9ebefbb88
Shakour <shakouralishahi@gmail.com>
2025-09-05 09:53:38 +0330

    Update backend/tests/unit_tests/test_exceptions.py

--- backend/tests/unit_tests/test_exceptions.py
+++ backend/tests/unit_tests/test_exceptions.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from backend.app.services.exceptions import (
+from app.services.exceptions import (
     GravityPMException,
     ValidationError,
     AuthenticationError,
